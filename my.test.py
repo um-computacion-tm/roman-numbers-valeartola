40378fad4167358214025983857837eaf4da7701
valeartola <mv.artola@alumno.um.edu.ar>
2023-04-06 11:08:48 -0300

    first comment

---
+++ my.test.py
@@ -0,0 +1,8 @@
+import unittest
+from romanos import decimal2roman
+class MyTest(unittest.TestCase):
+    def test_I(self):
+        self.assertEqual(decimal2roman(1), "I")
+
+if __name__== "__main__":
+    unittest.main()
